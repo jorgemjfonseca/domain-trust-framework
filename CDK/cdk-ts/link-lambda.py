706698732b57ed0c4887e46e88611fac4f6b388e
Jorge Fonseca <12628993+jorgemjfonseca@users.noreply.github.com>
2023-07-03 18:03:44 +0100

    .

--- CDK/cdk-ts/link-lambda.py
+++ CDK/cdk-ts/link-lambda.py
@@ -2,37 +2,42 @@ import os
 
 path = '.'
 
-target_files = [
-    os.path.join(root, name)
-    for root, dirs, files in os.walk(path)
-    for name in files
-    if '/cdk.out/' not in os.path.join(root, name) 
-    if '/LAMBDA/' not in os.path.join(root, name) 
-    if './lib/' in os.path.join(root, name) 
-    if name.endswith(".py")
-]
+def target_files(): 
+    return [
+        os.path.join(root, name)
+        for root, dirs, files in os.walk(path)
+        for name in files
+        if '/cdk.out/' not in os.path.join(root, name) 
+        if '/LAMBDA/' not in os.path.join(root, name) 
+        if './lib/' in os.path.join(root, name) 
+        if name.endswith(".py")
+    ]
 
-target_dirs = [
-    os.path.join(root, name)
-    for root, dirs, files in os.walk(path)
-    for name in dirs
-    if '/cdk.out/' not in os.path.join(root, name) 
-    if '/LAMBDA/' not in os.path.join(root, name) 
-    if './lib/' in os.path.join(root, name) 
-    if '/lambda/' in os.path.join(root, name) 
-]
+def target_dirs(): 
+    return [
+        os.path.join(root, name)
+        for root, dirs, files in os.walk(path)
+        for name in dirs
+        if '/cdk.out/' not in os.path.join(root, name) 
+        if '/LAMBDA/' not in os.path.join(root, name) 
+        if './lib/' in os.path.join(root, name) 
+        if '/lambda/' in os.path.join(root, name) 
+    ]
 
-source_files = [
-    os.path.join(root, name)
-    for root, dirs, files in os.walk(path)
-    for name in files
-    if './lib/Common/LAMBDA/python-layer/python/' in os.path.join(root, name) 
-    if name.endswith(".py")]
+def source_files(): 
+    return [
+        os.path.join(root, name)
+        for root, dirs, files in os.walk(path)
+        for name in files
+        if './lib/Common/LAMBDA/python-layer/python/' in os.path.join(root, name) 
+        if name.endswith(".py")
+    ]
 
 
 def IgnoreContent(files):
     # create ignore file.
     ignores = []
+    files.sort()
     for f in files:
         name = f.split('/')[-1]
         ignores.append(name)
@@ -49,28 +54,49 @@ def ReplaceFile(file, content):
 
 def MergeIgnores(ignore, dirs):
     print ('Merging ignores...')
+    dirs.sort()
     for f in dirs:
         ignore_path = f + '/.gitignore'
         ReplaceFile(ignore_path, ignore)
         #print(ignore_path)
 
-def LinkFiles(dirs, files):
+
+def LinkFiles(dirs, files, unlink=False):
     import subprocess
     print ('Linking files...')
+    dirs.sort()
+    files.sort()
     for dir in dirs:
         for src in files:
             name = src.split('/')[-1]
             dst = os.path.join(dir, name)
             #print(dst)
 
-            #os.symlink(src, dst)
-            base = '../../../../Common/LAMBDA/python-layer/python'
-            cmd = f'ln -s {base}/{name} {dst}'
-            print(cmd)
-            subprocess.Popen(cmd, shell=True)
-            return
+            cmd = ''
+            if unlink:
+                if os.path.exists(dst):
+                    cmd = f'rm {dst}'
+                    print(cmd)
+                    subprocess.Popen(cmd, shell=True)
+            else:
+                if not os.path.exists(dst):
+                    base = '../../../../Common/LAMBDA/python-layer/python'
+                    cmd = f'ln -s {base}/{name} {dst}'
+                    print(cmd)
+                    subprocess.Popen(cmd, shell=True)
+            
+            #return
         
+def Run():
+    files = source_files()
+    dirs = target_dirs()
+    
+    ignore = IgnoreContent(files)
+    MergeIgnores(ignore, dirs)
+    LinkFiles(dirs, files, unlink=False)
 
-ignore = IgnoreContent(files=source_files)
-MergeIgnores(ignore, dirs=target_dirs)
-LinkFiles(dirs=target_dirs, files=source_files)
+
+if not os.getcwd().endswith('/CDK/cdk-ts'):
+    print('Run this at ./domain-trust-framewor/CDK/cdk-ts')
+else:
+    Run()
