558b2a0fb0a8253af08d481e2817a5adc0feb5f4
Jorge Fonseca <12628993+jorgemjfonseca@users.noreply.github.com>
2023-07-03 17:08:46 +0100

    .

---
+++ CDK/cdk-ts/link-lambda.py
@@ -0,0 +1,46 @@
+import os
+
+path = '.'
+
+target_files = [
+    os.path.join(root, name)
+    for root, dirs, files in os.walk(path)
+    for name in files
+    if '/cdk.out/' not in os.path.join(root, name) 
+    if '/LAMBDA/' not in os.path.join(root, name) 
+    if './lib/' in os.path.join(root, name) 
+    if name.endswith(".py")
+]
+
+target_dirs = [
+    os.path.join(root, name)
+    for root, dirs, files in os.walk(path)
+    for name in dirs
+    if '/cdk.out/' not in os.path.join(root, name) 
+    if '/LAMBDA/' not in os.path.join(root, name) 
+    if './lib/' in os.path.join(root, name) 
+    if '/lambda/' in os.path.join(root, name) 
+]
+
+source_files = [
+    os.path.join(root, name)
+    for root, dirs, files in os.walk(path)
+    for name in files
+    if './lib/Common/LAMBDA/python-layer/python/' in os.path.join(root, name) 
+    if name.endswith(".py")]
+
+for f in source_files:
+    #print(f)
+    pass
+
+# create ignore file.
+ignores = []
+for f in source_files:
+    ignores.append(f.split('/')[-1])
+ignores.sort()
+ignore = '\n'.join(ignores)
+
+print ('Merging ignores...')
+for f in target_dirs:
+    ignore_path = f + '/.gitignore'
+    print(ignore_path)
